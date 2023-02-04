029213d87401d21362b77003522e03de1a59e397
EnrisKumi <ekumi20@epoka.edu.al>
2023-02-04 13:32:30 +0100

    get all posts

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'partials/header.php';
 
     <!--Posts-->
     <section class="posts">
-      <div class="container posts_container">
+      <div id="posts_container" class="container posts_container">
         <article class="post">
           <div class="post_thumbnail">
             <img src="./images/blog2.jpg" alt="Image not found" />
@@ -26,121 +26,7 @@ include 'partials/header.php';
           <div class="post_info">
             <a href="" class="category_button">Wild Life</a>
             <h3 class="post_title">
-              <a href="<?= ROOT_URL ?>/post.php"
-                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                et.</a
-              >
-            </h3>
-            <p class="post_body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              labore voluptatum quam fugiat consequuntur, accusantium dolorem id
-              repellendus facere iste?
-            </p>
-            <div class="post_author">
-              <div class="post_author-avatar">
-                <img src="./images/avatar3.jpg" alt="Image not found" />
-              </div>
-              <div class="post_author-info">
-                <h5>By: Test Test</h5>
-                <small>June, 12, 2022 - 07:23</small>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <div class="post_thumbnail">
-            <img src="./images/blog3.jpg" alt="Image not found" />
-          </div>
-          <div class="post_info">
-            <a href="" class="category_button">Wild Life</a>
-            <h3 class="post_title">
-              <a href="post.html"
-                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                et.</a
-              >
-            </h3>
-            <p class="post_body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              labore voluptatum quam fugiat consequuntur, accusantium dolorem id
-              repellendus facere iste?
-            </p>
-            <div class="post_author">
-              <div class="post_author-avatar">
-                <img src="./images/avatar3.jpg" alt="Image not found" />
-              </div>
-              <div class="post_author-info">
-                <h5>By: Test Test</h5>
-                <small>June, 12, 2022 - 07:23</small>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <div class="post_thumbnail">
-            <img src="./images/blog4.jpg" alt="Image not found" />
-          </div>
-          <div class="post_info">
-            <a href="" class="category_button">Wild Life</a>
-            <h3 class="post_title">
-              <a href="post.html"
-                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                et.</a
-              >
-            </h3>
-            <p class="post_body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              labore voluptatum quam fugiat consequuntur, accusantium dolorem id
-              repellendus facere iste?
-            </p>
-            <div class="post_author">
-              <div class="post_author-avatar">
-                <img src="./images/avatar3.jpg" alt="Image not found" />
-              </div>
-              <div class="post_author-info">
-                <h5>By: Test Test</h5>
-                <small>June, 12, 2022 - 07:23</small>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <div class="post_thumbnail">
-            <img src="./images/blog9.jpg" alt="Image not found" />
-          </div>
-          <div class="post_info">
-            <a href="" class="category_button">Wild Life</a>
-            <h3 class="post_title">
-              <a href="post.html"
-                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                et.</a
-              >
-            </h3>
-            <p class="post_body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              labore voluptatum quam fugiat consequuntur, accusantium dolorem id
-              repellendus facere iste?
-            </p>
-            <div class="post_author">
-              <div class="post_author-avatar">
-                <img src="./images/avatar3.jpg" alt="Image not found" />
-              </div>
-              <div class="post_author-info">
-                <h5>By: Test Test</h5>
-                <small>June, 12, 2022 - 07:23</small>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <div class="post_thumbnail">
-            <img src="./images/blog7.jpg" alt="Image not found" />
-          </div>
-          <div class="post_info">
-            <a href="" class="category_button">Wild Life</a>
-            <h3 class="post_title">
-              <a href="post.html"
-                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                et.</a
+              <a id="title" href="<?= ROOT_URL ?>/post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,et.</a
               >
             </h3>
             <p class="post_body">
@@ -174,11 +60,10 @@ include 'partials/header.php';
         <a href="" class="category_button">Music</a>
       </div>
     </section>
-    <button id="click">click</button>
     <!--End Of Category-->
 
     <script src="./js/main.js"></script>
-    <script src="./js//posts//getAllPosts.js"></script>
+    <script src="./js/posts/getAllPosts.js"></script>
 
   </body>
 </html>
--- js/posts/getAllPosts.js
+++ js/posts/getAllPosts.js
@@ -1,18 +1,95 @@
-const click = document.querySelector('#click');
+window.onload = function () {
+  getData();
+};
 
-click.addEventListener('click', getData );
+async function getData() {
+  await fetch("http://localhost:81/webproject/api/posts/getPosts.php", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const postsContainer = document.querySelector("#posts_container");
+      console.log(postsContainer);
+      data.forEach((element) => {
+        const article = document.createElement("article");
+        article.className = "post";
 
+        const postThumbnail = document.createElement("div");
+        postThumbnail.className = "post_thumbnail";
 
-function getData(){
-    fetch('http://localhost:81/webproject/api/posts/getPosts.php',{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then((response) => response.json())
-    .then((data)=> console.log(data))
+        const imagePost = document.createElement("img");
+        imagePost.src = "./images/blog2.jpg"
+        imagePost.alt = "Image not found"
+
+        postThumbnail.appendChild(imagePost);
+
+        article.appendChild(postThumbnail);
+
+        const postInfo = document.createElement("div");
+        postInfo.className = "post_info";
+
+        const categoryLink = document.createElement("a");
+        const y = document.createTextNode(`${element.categoryId}`)
+        categoryLink.appendChild(y);
+        categoryLink.className = "category_button";
+
+        postInfo.appendChild(categoryLink);
+
+        const postTitle = document.createElement("h3");
+        postTitle.className = "post_title";
+
+        const titleLink = document.createElement("a");
+        const x = document.createTextNode(`${element.title}`)
+        titleLink.setAttribute("href", "http://localhost:81/webproject/post.php")
+        titleLink.appendChild(x);
+        postTitle.appendChild(titleLink);
+
+        postInfo.appendChild(postTitle);
+
+        const body = document.createElement("p");
+        body.innerHTML = element.body
+        body.className = "post_body";
+
+        postInfo.appendChild(body);
+
+        const authorDiv = document.createElement("div");
+        authorDiv.className = "post_author";
+
+        const authorAvatar = document.createElement("div");
+        authorAvatar.className = "post_author-avatar";
+
+        const authorAvatarImage = document.createElement("img");
+        authorAvatarImage.src = "./images/blog2.jpg"
+        authorAvatarImage.alt = "Image not found"
+        authorAvatar.appendChild(authorAvatarImage);
+
+        authorDiv.appendChild(authorAvatar);
+
+        const authorInfo = document.createElement("div");
+        authorInfo.className = "post_author-info";
+
+        const authorName = document.createElement("h5");
+        authorName.innerHTML = `By: ${element.userId}`;
+        authorInfo.appendChild(authorName);
+
+        const datePost = document.createElement("small");
+        datePost.innerHTML = element.dateTime
+        authorInfo.appendChild(datePost);
+
+        authorDiv.appendChild(authorInfo);
+
+        postInfo.appendChild(authorDiv);
+
+        article.appendChild(postInfo);
+
+        postsContainer.appendChild(article)
+        console.log(postsContainer)
+
+      });
+    });
 }
 
-window.onload = function() {
-    getData();
-  };
